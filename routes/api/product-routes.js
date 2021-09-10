64b65ea98d31c69d4ba5e3b0ef0f49a6153a58bd
TeachY0u <marianoteach@rocketmail.com>
2021-09-09 21:13:37 -0700

    adding delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,8 +114,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+      });
+    if (!productData) {
+      res.status(404).json({
+        message: 'No Product found with this id'
+      });
+      return;
+    }
+  }
 });
 
 module.exports = router;
